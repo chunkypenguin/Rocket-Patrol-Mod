b934a00994a779ae2b5377b59634dcf05a7b82f5
chunkypenguin <abeteta@ucsc.edu>
2024-02-01 00:19:45 -0800

    Main.js comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+//Alex Beteta
+//Title: Rocket Wars
+//Approximate time to create: 12 hours
+//List of Mods & point values
+// +1 Implement speed increase half way through game time
+// +1 Randomize each spaceship's movement direction (every time a ship is destroyed)
+// +3 Display time remaining (in seconds)
+// +5 create a new enemy spaceship (new art, smaller, moves faster, more points)
+//Custom Mods
+// +5 create another new enemy spaceship that takes multiple hits(new art, larger, moves slower, lots of points, has randomized HP (like supersmash bros ball!))
+// +5 simultaneous two-player VS mode with custom scoring system, UI and win screen
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
